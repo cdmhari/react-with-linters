d28816a0cc2ed6764a9cc598800101ecb3692c2e
haprabhakar <harikrishnan.prabhakar@freshworks.com>
2021-06-24 16:35:46 +0530

    test lint

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { hot } from "react-hot-loader/root";
 
-class App extends React.Component {
-  render() {
-    const { name } = this.props;
-    return (
-      <>
-        <h1>Hi {name}</h1>
-      </>
-    );
+  class App extends React.Component {
+    render() {
+      const { name } = this.props;
+      return (
+        <>
+          <h1>Hi {name}</h1>
+        </>
+      );
+    }
   }
-}
 
 export default hot(App);
