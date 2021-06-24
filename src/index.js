db6d01ae0599353d07fb45d1c8bdcdebb61c6bb2
haprabhakar <harikrishnan.prabhakar@freshworks.com>
2021-06-24 15:42:31 +0530

    name change

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles.scss";
 
-ReactDOM.render(<App name="Harikrishnan" />, document.body);
+ReactDOM.render(<App name="Harikrishnan Prabhakar" />, document.body);
